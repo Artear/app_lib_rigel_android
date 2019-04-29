0325085ef7f904a04567640d17ad0801799ad6f6
sergio.banares <sergio.banares@gmail.com>
2019-04-29 16:17:51 -0300

    Change: Rearrange & Reformat Code & Test Imports Fix AndroidX

--- app/src/main/java/com/artear/rigelexample/BaseActivity.kt
+++ app/src/main/java/com/artear/rigelexample/BaseActivity.kt
@@ -1,11 +1,7 @@
 package com.artear.rigelexample
 
-import android.os.Bundle
 import com.artear.rigel.NavigationActivity
 
 
 abstract class BaseActivity : NavigationActivity() {
-   override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 }
--- app/src/main/java/com/artear/rigelexample/MainActivity.kt
+++ app/src/main/java/com/artear/rigelexample/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : BaseActivity() {
     override var navigationProvider: NavigationProvider =
             NavigationProvider(ExampleSection.values(), R.id.menu_first)
 
-    override fun onBottomNavigationViewCreated(bottomNavigationView: BottomNavigationView){
+    override fun onBottomNavigationViewCreated(bottomNavigationView: BottomNavigationView) {
         bottomNavigationView.apply {
             inflateMenu(R.menu.menu_bottom_navigation)
             itemIconTintList = AppCompatResources.getColorStateList(context, R.color.navigation_item_icon)
